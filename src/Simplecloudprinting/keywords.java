dd839162fa504c996901792ec0f21a2d2ea8be70
jajimiramya <36047022+jajimiramya@users.noreply.github.com>
2018-02-04 12:00:53 +0530

    commiting project files
    Commiting all project related files

---
+++ src/Simplecloudprinting/keywords.java
@@ -0,0 +1,58 @@
+package Simplecloudprinting;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class keywords 
+{
+	static FirefoxDriver driver;
+	static Properties prop;
+	static FileInputStream file;
+	
+	public void openbrowser() throws IOException 
+	{
+	driver=new 	FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+	 file=new FileInputStream("G:\\Ramya\\workspace2\\PrintOnGo\\src\\objectrepository\\objectrepository.properties");
+	 prop=new Properties();
+	prop.load(file);
+	}
+	public void navigate(String data) 
+	{
+	driver.get(data);
+		
+	}
+	public void click(String objectname) 
+	{
+		driver.findElement(By.id(prop.getProperty(objectname))).click();
+	}
+	public void input(String data, String objectname) 
+	{
+		driver.findElement(By.xpath(prop.getProperty(objectname))).sendKeys(data);	
+		
+	}
+	public void select(String data) throws AWTException 
+	{
+		StringSelection selection=new StringSelection(data);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+		Robot robot=new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_V);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+	}
+	}
+
---
+++ src/Simplecloudprinting/printing.java
@@ -0,0 +1,132 @@
+package Simplecloudprinting;
+
+import java.awt.AWTException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.xpath.compiler.Keywords;
+
+public class printing
+{
+	public static void main(String[] args) throws IOException, AWTException 
+	{
+		keywords key=new keywords();
+		
+		ArrayList a=new ArrayList();
+		FileInputStream file=new FileInputStream("G:\\Ramya\\workspace2\\PrintOnGo\\src\\testcases\\printing.xlsx");
+		XSSFWorkbook wbks=new XSSFWorkbook(file);
+		Sheet s= wbks.getSheet("Teststeps");
+		Iterator itr = s.iterator();
+		while(itr.hasNext())
+		{
+		Row rowitr=(Row)itr.next();
+		Iterator cellitr=rowitr.cellIterator();
+		while(cellitr.hasNext())
+		{
+			Cell Celldata=(Cell)cellitr.next();
+	
+		switch(Celldata.getCellType())
+		{
+		case Cell.CELL_TYPE_STRING:
+			a.add(Celldata.getStringCellValue());
+			break;
+		case Cell.CELL_TYPE_NUMERIC:
+			a.add(Celldata.getNumericCellValue());
+			break;
+		case Cell.CELL_TYPE_BOOLEAN:
+			a.add(Celldata.getBooleanCellValue());
+			break;
+		}
+		}
+		}
+              for (int i=0;i<a.size();i++)
+              {
+			
+			if (a.get(i).equals("openbrowser")) 
+			{
+				String keyword=(String)a.get(i);
+				String data=(String)a.get(i+1);
+				String objectname=(String)a.get(i+2);
+				String runmode=(String)a.get(i+3);
+				System.out.println(keyword);
+				System.out.println(data);
+				System.out.println(objectname);
+				System.out.println(runmode);
+				if(runmode.equals("yes"))
+				{
+				key.openbrowser();
+			    }
+			}
+			if (a.get(i).equals("navigate")) 
+			{
+				String keyword=(String)a.get(i);
+				String data=(String)a.get(i+1);
+				String objectname=(String)a.get(i+2);
+				String runmode=(String)a.get(i+3);
+				System.out.println(keyword);
+				System.out.println(data);
+				System.out.println(objectname);
+				System.out.println(runmode);
+				if(runmode.equals("yes"))
+				{
+				key.navigate(data);
+			    }
+			}
+			if (a.get(i).equals("click")) 
+			{
+				String keyword=(String)a.get(i);
+				String data=(String)a.get(i+1);
+				String objectname=(String)a.get(i+2);
+				String runmode=(String)a.get(i+3);
+				System.out.println(keyword);
+				System.out.println(data);
+				System.out.println(objectname);
+				System.out.println(runmode);
+				if(runmode.equals("yes"))
+				{
+				key.click(objectname);
+			    }
+			}
+			if (a.get(i).equals("input")) 
+			{
+				String keyword=(String)a.get(i);
+				String data=(String)a.get(i+1);
+				String objectname=(String)a.get(i+2);
+				String runmode=(String)a.get(i+3);
+				System.out.println(keyword);
+				System.out.println(data);
+				System.out.println(objectname);
+				System.out.println(runmode);
+				if(runmode.equals("yes"))
+				{
+					key.input(data,objectname);
+			    }
+			}
+			if (a.get(i).equals("select")) 
+			{
+				String keyword=(String)a.get(i);
+				String data=(String)a.get(i+1);
+				String objectname=(String)a.get(i+2);
+				String runmode=(String)a.get(i+3);
+				System.out.println(keyword);
+				System.out.println(data);
+				System.out.println(objectname);
+				System.out.println(runmode);
+				if(runmode.equals("yes"))
+				{
+				key.select(data);
+			    }
+			}
+			
+			
+              }
+	}
+}
+			
+			
